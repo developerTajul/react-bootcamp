a551654c98e9d789b4348a2cd30f1c30bf15e6b6
developerTajul <developer.tajul@gmail.com>
2021-08-07 07:39:20 +0600

    counter app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class App extends React.Component{
+class App extends Component{
+    state = {
+        count : 10,
+    }
+
+    incrementCount = () => {
+        this.setState({
+            count: this.state.count + 1
+        });
+    }
+
+
+    decrementCount = () => {
+        if(this.state.count > 0){
+            this.setState({
+                count: this.state.count - 1
+            });
+        }else{
+
+        }
+    }
+
+
     render(){
-        return <h1>Hello From create app</h1>
+        const { count } = this.state;
+        const { incrementCount,  decrementCount } = this;
+        return(
+             
+                <div className="counter-area">
+                    <h1>Counter App</h1>
+                    <p>Counter - {count}</p>
+                    <button onClick={incrementCount}>Increment</button>
+                    <button  onClick={decrementCount}>Decrement</button>
+                    <IsOddOrEven count={count} />
+                </div>
+        )
+    }
+}
+
+class IsOddOrEven extends Component{
+    render(){
+        const numberState = this.props.count % 2 === 0 ? 'Even' : 'Odd';
+        return(
+            <>
+                <p> {this.props.count} is {numberState}</p>
+            </>
+        )
     }
 }
 
